061150402b4f82097312084e6831f0c1112349ad
Matthew Knudsen <mknudsen01@gmail.com>
2020-09-10 10:53:20 -0400

    Export required components and providers from package

---
+++ index.js
@@ -0,0 +1,17 @@
+import P from "./components/P";
+import Flex from "./components/Flex";
+import Box from "./components/Box";
+import Heading from "./components/Heading";
+import Image from "./components/Image";
+import InViewAnimation from "./components/InViewAnimation";
+import { CurrentBreakpointProvider } from "./contexts/CurrentBreakpoint";
+
+export {
+  P,
+  Flex,
+  Box,
+  InViewAnimation,
+  Heading,
+  Image,
+  CurrentBreakpointProvider,
+};
